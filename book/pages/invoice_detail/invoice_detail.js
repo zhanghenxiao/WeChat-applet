067ce6c7d8ebaa17347c1c82b37b8d29af6854d1
Cookie <Cookie@inventec.com>
2020-06-22 18:52:17 +0800

    [新增]新增页面数据传入下个页面(失败)

--- book/pages/invoice_detail/invoice_detail.js
+++ book/pages/invoice_detail/invoice_detail.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputlist : []
+    inputlist : [],
+    fpdm:'051001900111',
+    fphm:'51096586',
+    kprq:"20190608",
   },
   bindKeyInput: function (e) {
     console.log(e)
@@ -34,29 +37,61 @@ Page({
       })
     }
   },
-send : function(e){
-      console.log(1)
-      wx.request({
-        url: "https://fapiao.market.alicloudapi.com/v2/invoice/query",
-        method:'POST',
-        data: {
-          fpdm:'051001900111',
-          fphm:'51096586',
-          kprq:'20190608',
-        },
-        header: { 
-          // Authorization:"APPCODE d86557f39f304530bd642da241c0104c",
-          Authorization:'APPCODE d86557f39f304530bd642da241c0104c',
-        },
-        success: function (res) {
-          console.log(res.data)
-        },
-        fail:function (res) {
-          console.log("fail")
-        },
-      })
+
+json2Form:function(json) {
+  var str = [];
+  for (var p in json) {
+  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]));
+  }
+  return str.join("&");
   },
-  
+//fpdm: "4400151320",
+// fphm: "39915391",
+// kprq: "20160301",
+// checkCode: "853924",
+
+//3700172320
+//16885448
+//20180626
+//348364
+
+//011001600111
+//15540964
+//20170613
+//916908
+
+//052001600111
+//32176424
+//20190212
+//227590
+
+
+send : function(){
+wx.request({
+    url: 'https://fapiao.market.alicloudapi.com/v2/invoice/query', 
+    data: this.json2Form({ 
+    fpdm: this.data.mind1,
+    fphm: this.data.mind2,
+    kprq: this.data.mind3,
+    checkCode: this.data.mind4, //根据发票类型填对应的参数
+  }),
+  header: {
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    Authorization: "APPCODE d86557f39f304530bd642da241c0104c",
+    // Host: "fapiao.market.alicloudapi.com",
+  },
+  success(res) {
+    console.log(res.data)
+    // console.log(1)
+    wx.navigateTo({
+      url:'../invoice_verify/invoice_verify?data='+ res.data
+    })
+  },
+  method: "post",
+  })
+
+},
+
   /**
    * 生命周期函数--监听页面加载
    */
